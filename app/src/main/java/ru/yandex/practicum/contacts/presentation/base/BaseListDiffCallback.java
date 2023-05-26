3f7206ffc0d71354e3cb75dc6a3b85f693173bba
simsasha <alespada21@gmail.com>
2023-05-26 11:24:15 +0300

    Первая попытка. ПР№ 2

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -11,13 +11,13 @@ public class BaseListDiffCallback<T extends ListDiffInterface> extends DiffUtil.
         return newItem;
     }
 
-    //@Override
+    @Override
     public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
         return oldItem.theSameAs(newItem);
     }
 
     @Override
     public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
-        return false;
+        return oldItem.equals(newItem);
     }
 }
